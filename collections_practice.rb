8f68f1076a8ea99fff1029ebf33b18727232061c
Alec German <alec_german@yahoo.com>
2018-02-05 17:01:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.collect {|item| item[0..1] + '$' + item[3..item.length-1]}
+end
+
+
+def find_a(array)
+  array.select {|element| element.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.collect.with_index{|x, index| index != 1 ? x + 's' : x}
+end
